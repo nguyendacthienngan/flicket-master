7bc7e446a922cc0bdfaa77fc4ff063d158fe242f
nguyendacthienngan <nguyendacthienngan@gmail.com>
2023-10-11 16:54:45 +0700

    Update torch utils and gitignore

--- application/utils/torch_utils.py
+++ application/utils/torch_utils.py
@@ -4,45 +4,50 @@
 # Preprocess the image and convert it to a torch tensor
 # Do the prediction
 
-#https://www.python-engineer.com/posts/pytorch-model-deployment-with-flask/
+# https://www.python-engineer.com/posts/pytorch-model-deployment-with-flask/
 import io
-import torch 
-import torch.nn as nn 
-from transformers import XLNetTokenizer, XLNetForSequenceClassification, XLNetModel
-from keras.preprocessing.sequence import pad_sequences
+import torch
+from transformers import XLNetTokenizer, XLNetForSequenceClassification
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 MAX_LEN = 512
 class_names = ['negative', 'positive']
 PRE_TRAINED_MODEL_NAME = 'xlnet-base-cased'
 
-model = XLNetForSequenceClassification.from_pretrained(PRE_TRAINED_MODEL_NAME, num_labels = 2)
-model.load_state_dict(torch.load('./models/xlnet_model.bin'))
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
+model = XLNetForSequenceClassification.from_pretrained(
+    PRE_TRAINED_MODEL_NAME, num_labels=2)
+model.load_state_dict(torch.load(
+    'xlnet_model.bin', map_location=torch.device('cpu')))
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+model = model.to("cpu")
 tokenizer = XLNetTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+
 
 def predict_sentiment(text):
     review_text = text
 
     encoded_review = tokenizer.encode_plus(
-    review_text,
-    max_length=MAX_LEN,
-    add_special_tokens=True,
-    return_token_type_ids=False,
-    pad_to_max_length=False,
-    return_attention_mask=True,
-    return_tensors='pt',
+        review_text,
+        max_length=MAX_LEN,
+        add_special_tokens=True,
+        return_token_type_ids=False,
+        pad_to_max_length=False,
+        return_attention_mask=True,
+        return_tensors='pt',
     )
 
-    input_ids = pad_sequences(encoded_review['input_ids'], maxlen=MAX_LEN, dtype=torch.Tensor ,truncating="post",padding="post")
-    input_ids = input_ids.astype(dtype = 'int64')
-    input_ids = torch.tensor(input_ids) 
+    input_ids = pad_sequences(
+        encoded_review['input_ids'], maxlen=MAX_LEN, dtype=torch.Tensor, truncating="post", padding="post")
+    input_ids = input_ids.astype(dtype='int64')
+    input_ids = torch.tensor(input_ids)
 
-    attention_mask = pad_sequences(encoded_review['attention_mask'], maxlen=MAX_LEN, dtype=torch.Tensor ,truncating="post",padding="post")
-    attention_mask = attention_mask.astype(dtype = 'int64')
-    attention_mask = torch.tensor(attention_mask) 
+    attention_mask = pad_sequences(
+        encoded_review['attention_mask'], maxlen=MAX_LEN, dtype=torch.Tensor, truncating="post", padding="post")
+    attention_mask = attention_mask.astype(dtype='int64')
+    attention_mask = torch.tensor(attention_mask)
 
-    input_ids = input_ids.reshape(1,512).to(device)
+    input_ids = input_ids.reshape(1, 512).to(device)
     attention_mask = attention_mask.to(device)
 
     outputs = model(input_ids=input_ids, attention_mask=attention_mask)
@@ -50,7 +55,7 @@ def predict_sentiment(text):
     outputs = outputs[0][0].cpu().detach()
 
     probs = F.softmax(outputs, dim=-1).cpu().detach().numpy().tolist()
-    _, prediction = torch.max(outputs, dim =-1)
+    _, prediction = torch.max(outputs, dim=-1)
 
     print("Positive score:", probs[1])
     print("Negative score:", probs[0])
